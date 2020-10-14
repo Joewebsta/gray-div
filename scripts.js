1a28c602ffaf759d3a63d5230c888ad410bb7e84
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-14 13:07:34 -0400

    Click square to change bgColor from gray to pink and back again

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('it works!');
+let grayDiv = document.querySelector('.square');
+
+grayDiv.addEventListener('click', changeColor);
+
+function changeColor() {
+  grayDiv.classList.toggle('gray');
+  grayDiv.classList.toggle('pink');
+}
